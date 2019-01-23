56011ff64eaf62d22242e3d2ea3697c14f184043
ankerek <ankerek@gmail.com>
2019-01-23 22:56:08 +0100

    setup bundle analyzer

--- packages/expense-tracker-client/webpack.prod.js
+++ packages/expense-tracker-client/webpack.prod.js
@@ -1,7 +1,14 @@
 const merge = require('webpack-merge')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    }),
+  ],
 })
