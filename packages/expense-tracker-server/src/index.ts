14a3e61446216b6311930c732f4e767bed62f9e2
ankerek <ankerek@gmail.com>
2019-01-29 20:31:14 +0100

    add serve script

--- packages/expense-tracker-server/src/index.ts
+++ packages/expense-tracker-server/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import { ApolloServer } from 'apollo-server-express'
 import { databaseInitializer } from './databaseInitializer'
-import { asyncMiddleware } from './utils/asyncMiddleware'
 import { buildSchema } from './utils/buildSchema'
 import { getCurrentUser } from './utils/authentification'
+
+const BUILD_PATH = path.join(
+  __dirname,
+  '..',
+  '..',
+  'expense-tracker-client',
+  'dist'
+)
 
 dotenv.config()
 const port = process.env.API_PORT
@@ -13,10 +21,6 @@ const bootstrap = async () => {
   await databaseInitializer()
 
   const app = express()
-
-  app.listen(port, () => {
-    console.log(`server is listening on ${port}`)
-  })
 
   const schema = await buildSchema()
 
@@ -32,16 +36,27 @@ const bootstrap = async () => {
   })
   server.applyMiddleware({ app, path: '/graphql' })
 
-  app.get(
-    '/test',
-    asyncMiddleware(async (req, res, next) => {
-      /*
-      if there is an error thrown in getUserFromDb, asyncMiddleware
-      will pass it to next() and express will handle the error;
-    */
-      res.send('hello world')
-    })
-  )
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(BUILD_PATH))
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(path.join(BUILD_PATH, 'index.html')))
+    )
+  }
+
+  app.listen(port, () => {
+    console.log(`server is listening on ${port}`)
+  })
+
+  // app.get(
+  //   '/test',
+  //   asyncMiddleware(async (req, res, next) => {
+  //     /*
+  //     if there is an error thrown in getUserFromDb, asyncMiddleware
+  //     will pass it to next() and express will handle the error;
+  //   */
+  //     res.send('hello world')
+  //   })
+  // )
 }
 
 bootstrap().catch(err => console.log(err))
