f1185f6e26934c5b5a376f20df1d4d7efb9986d0
ankerek <ankerek@gmail.com>
2019-03-24 21:05:50 +0100

    move init actions to private route

--- packages/expense-tracker-client/src/core-components/PrivateRoute/index.tsx
+++ packages/expense-tracker-client/src/core-components/PrivateRoute/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
 import { Redirect, Route, RouteProps } from 'react-router'
 import { GetCurrentUser } from '@controllers/user/GetCurrentUser'
-import { GetIsOnline } from '@controllers/network/GetIsOnline'
+import { GetIsOnline, getIsOnlineQuery } from '@controllers/network/GetIsOnline'
+import { client } from '@apollo/initializeApollo'
+import { restoreLocalOperations } from '@controllers/network/localOperations'
+import { getAccountListQuery } from '@controllers/account/GetAccountList'
+import { getCategoryListQuery } from '@controllers/category/GetCategoryList'
+import { offlineLink } from '@apollo/offlineLink'
+
+class InitActions extends React.Component {
+  componentDidMount() {
+    const isOnlineData = client.readQuery({ query: getIsOnlineQuery })
+    if (!isOnlineData.isOnline) {
+      offlineLink.close()
+    }
+
+    restoreLocalOperations(client)
+
+    client.query({
+      query: getAccountListQuery,
+      fetchPolicy: 'network-only',
+    })
+
+    client.query({
+      query: getCategoryListQuery,
+      fetchPolicy: 'network-only',
+    })
+  }
+
+  render() {
+    return this.props.children
+  }
+}
 
 export class PrivateRoute extends React.Component<RouteProps> {
   render() {
@@ -17,7 +47,11 @@ export class PrivateRoute extends React.Component<RouteProps> {
               >
                 {({ data, loading }) => {
                   if (data && data.getCurrentUser) {
-                    return <Component {...this.props} />
+                    return (
+                      <InitActions>
+                        <Component {...this.props} />
+                      </InitActions>
+                    )
                   }
 
                   if (loading) {
--- packages/expense-tracker-client/src/index.tsx
+++ packages/expense-tracker-client/src/index.tsx
@@ -4,9 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { client, waitForCache } from '@apollo/initializeApollo'
 import { registerServiceWorker } from './registerServiceWorker'
-import { restoreLocalOperations } from '@controllers/network/localOperations'
-import { getIsOnlineQuery } from '@controllers/network/GetIsOnline'
-import { offlineLink } from '@apollo/offlineLink'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from '@core-components/ThemeProvider'
@@ -25,12 +22,6 @@ const App = () => (
 )
 
 waitForCache.then(() => {
-  const isOnlineData = client.readQuery({ query: getIsOnlineQuery })
-  if (!isOnlineData.isOnline) {
-    offlineLink.close()
-  }
-
-  restoreLocalOperations(client)
   ReactDOM.render(<App />, document.getElementById('root'))
 })
 
