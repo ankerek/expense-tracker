bd53881b130cc84d6e9d9630723bc831ed761643
ankerek <ankerek@gmail.com>
2019-04-24 01:36:16 +0200

    update category expense chart

--- packages/expense-tracker-client/src/pages/OverviewPage/components/CategoriesExpenses/Chart.tsx
+++ packages/expense-tracker-client/src/pages/OverviewPage/components/CategoriesExpenses/Chart.tsx
@@ -1,27 +1,67 @@
 import React, { PureComponent } from 'react'
-import { PieChart, Pie, Legend, Tooltip } from 'recharts'
+import { PieChart, Pie, PieLabelRenderProps } from 'recharts'
 import { FormattedAmount } from '@components/FormattedAmount'
 
+const LabelRenderer: React.FC<PieLabelRenderProps> = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  name,
+  value,
+}) => {
+  const RADIAN = Math.PI / 180
+  const radius =
+    25 + Number(innerRadius) + (Number(outerRadius) - Number(innerRadius))
+  const x = Number(cx) + radius * Math.cos(-midAngle * RADIAN)
+  const y = Number(cy) + radius * Math.sin(-midAngle * RADIAN)
+
+  return (
+    <g>
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+        fontSize={13}
+      >
+        {name}
+      </text>
+      <text
+        x={x}
+        y={y}
+        dy={16}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+        fontSize={12}
+      >
+        ({value * -1}
+        <FormattedAmount amount={value * -1} />)
+      </text>
+    </g>
+  )
+}
+
 interface ChartProps {
-  data?: object[]
+  data?: Array<{ name: string; value: number }>
 }
 
 export class Chart extends PureComponent<ChartProps> {
-  tooltipFormatter = (value: number) => {
-    return <FormattedAmount amount={value * -1} />
-  }
-
   render() {
     const { data } = this.props
     return (
-      <PieChart width={200} height={200}>
+      <PieChart width={320} height={250}>
         <Pie
           dataKey="value"
+          outerRadius={50}
           isAnimationActive={false}
           data={data}
           fill="#8884d8"
+          label={LabelRenderer}
         />
-        <Tooltip formatter={this.tooltipFormatter} />
       </PieChart>
     )
   }
